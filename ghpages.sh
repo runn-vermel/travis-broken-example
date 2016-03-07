edf887b8db19317536af93e30a645fc26873bc03
Runn Vermel <runn.vermel@ge.com>
2016-03-07 15:01:03 -0800

    adding script to the mix

---
+++ ghpages.sh
@@ -0,0 +1,13 @@
+ #!/bin/bash
+
+git checkout gh-pages #move to the right branch
+rm -rf $COMPONENT #remove the older instance we are forwarding to from the main index.html
+git clone https://github.com/predixdev/$COMPONENT  #clone the new and improved repo
+cd $COMPONENT #go into component
+rm -rf .git #and remove the .git folder to avoid submodules
+cd ..
+
+#and commit and push the gh-pages branch.
+git add .
+git commit -m "updated component"
+git push origin gh-pages
